794f8e49defa8a7f06f72e9d51f9fdb00b7b3937
tranhuonghg21-dev <tranhuonghg21@gmail.com>
2025-12-10 10:49:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Trần Thị Hường - Trường TH Nguyễn Trãi - Phường Hà Giang 1 - ĐT: 0944604867 </div>", unsafe_allow_html=True)
