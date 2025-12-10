5957b343d5550790476afb268c5152baa8ed8a46
tranhuonghg21-dev <tranhuonghg21@gmail.com>
2025-12-10 11:06:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Trần Thị Hường - Trường Tiểu học Nguyễn Trãi Phường Hà Giang 1 - ĐT: 0944604867</p>
 </div>
 """, unsafe_allow_html=True)
 
